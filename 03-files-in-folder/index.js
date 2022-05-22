f462a3bad9176ad1e52e54b7071384979f04ddc2
Pavel <you@example.com>
2022-05-22 15:40:59 +0300

    fix: add recursion in 3d task (22.05.22 15:40)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,33 +3,21 @@ const path = require('path');
 const way = path.join(__dirname, 'secret-folder')
 const { stdin, stdout } = process;
 
-const files = fs.readdirSync(way, { withFileTypes: true }, (err) => { })
-
-files.forEach(e => {
-    if (e.isFile() === true) {
-        let weight = fs.statSync(
-            `${way}\\${e.name}`, (err) => { }
-        )
-        if (e.name.split('.')[0] === '') {
-            stdout.write(`.${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
-        } else {
-            stdout.write(`${e.name.split('.')[0]} - ${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
-        }
-        stdout.write('\n')
-    }
-    if (e.isFile() === false) {
-        fs.readdirSync(`${way}\\${e.name}`, { withFileTypes: true }, (err) => { }).forEach(el => {
-            if (el.isFile() === true) {
-                let weight = fs.statSync(
-                    `${way}\\${e.name}\\${el.name}`, (err) => { }
-                )
-                if (el.name.split('.')[0] === '') {
-                    stdout.write(`.${el.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
-                } else {
-                    stdout.write(`${el.name.split('.')[0]} - ${el.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
-                }
-                stdout.write('\n')
+function findingFiles(way) {
+    fs.readdirSync(way, { withFileTypes: true }, (err) => { }).forEach(e => {
+        if (e.isFile() === true) {
+            let weight = fs.statSync(`${way}\\${e.name}`, (err) => { })
+            if (e.name.split('.')[0] === '') {
+                stdout.write(`.${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+            } else {
+                stdout.write(`${e.name.split('.')[0]} - ${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
             }
-        })
-    }
-})
+            stdout.write('\n')
+        }
+        if (e.isFile() === false) {
+            findingFiles(`${way}\\${e.name}`)
+        }
+    })
+}
+
+findingFiles(way)
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
+
+function findingFiles(way) {
+    fs.readdirSync(way, { withFileTypes: true }, (err) => { }).forEach(e => {
+        if (e.isFile() === true) {
+            let weight = fs.statSync(`${way}\\${e.name}`, (err) => {})
+            stdout.write(`${e.name.split('.')[0]} - ${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+            stdout.write('\n')
+        }
+        if (e.isFile() === false) {
+            findingFiles(`${way}\\${e.name}`)
+        }
+    })
+}
+
+findingFiles(path.dirname(__filename))
