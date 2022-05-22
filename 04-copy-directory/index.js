7b360a0c43fbf3eac8590661b1ddc730e6e06e9d
Pavel <you@example.com>
2022-05-22 03:43:27 +0300

    feat: complete 4st task (22.05.22 3:43)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
 
+fs.mkdir(
+    path.join(__dirname, 'files-copy'),
+    err => {}
+)
+
+const files = fs.readdirSync(path.join(__dirname, 'files'),{ withFileTypes: true }, (err) => {})
+files.forEach(e => {
+    fs.copyFile(
+        path.join(__dirname, 'files', e.name),
+        path.join(__dirname, 'files-copy', e.name),
+        err => {}
+    )  
+})
