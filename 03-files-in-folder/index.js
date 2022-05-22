b3749f433b79cb6d866d3ab51a812f6715b4994d
Pavel <you@example.com>
2022-05-22 15:24:20 +0300

    fix: add subfolders in 3d task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,14 +3,33 @@ const path = require('path');
 const way = path.join(__dirname, 'secret-folder')
 const { stdin, stdout } = process;
 
-const files = fs.readdirSync(way,{ withFileTypes: true }, (err) => {})
+const files = fs.readdirSync(way, { withFileTypes: true }, (err) => { })
 
 files.forEach(e => {
     if (e.isFile() === true) {
         let weight = fs.statSync(
             `${way}\\${e.name}`, (err) => { }
         )
-        stdout.write(`${e.name.split('.')[0]} - ${e.name.split('.')[1]} - ${(weight.size/1024).toFixed(3)}kb`)
+        if (e.name.split('.')[0] === '') {
+            stdout.write(`.${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+        } else {
+            stdout.write(`${e.name.split('.')[0]} - ${e.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+        }
         stdout.write('\n')
+    }
+    if (e.isFile() === false) {
+        fs.readdirSync(`${way}\\${e.name}`, { withFileTypes: true }, (err) => { }).forEach(el => {
+            if (el.isFile() === true) {
+                let weight = fs.statSync(
+                    `${way}\\${e.name}\\${el.name}`, (err) => { }
+                )
+                if (el.name.split('.')[0] === '') {
+                    stdout.write(`.${el.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+                } else {
+                    stdout.write(`${el.name.split('.')[0]} - ${el.name.split('.')[1]} - ${(weight.size / 1024).toFixed(3)}kb`)
+                }
+                stdout.write('\n')
+            }
+        })
     }
 })
