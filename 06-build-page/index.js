c5e258cb12172b0c5ba2ebd9d52195532749baf9
Pavel <you@example.com>
2022-05-25 21:55:18 +0300

    fix: edit links for MacOS (25.05.22 21:55)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,32 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const { exit } = require('process');
 const way = path.join(__dirname, 'components')
 const out = path.join(__dirname, 'project-dist')
-
 fs.mkdir(out, err => { })
-fs.mkdir(`${out}\\assets`, err => { })
-fs.writeFile(`${out}\\index.html`, '', err => { });
+fs.mkdir(path.join(out, 'assets'), err => { })
+fs.writeFile(path.join(out, 'index.html'), '', err => { });
 
 ///////////////////////////////////////////html/////////////////////////////////////////////
 
-const stream = fs.createReadStream(`${__dirname}\\template.html`, 'utf-8');
+const stream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
 let temp = '';
 stream.on('data', chunk => temp += chunk);
 stream.on('end', () => {
     temp.split('{{').forEach((e, i) => {
         if (i === 0) {
-            fs.appendFile(`${out}\\index.html`, e, err => { })
+            fs.appendFile(path.join(out, 'index.html'), e, err => {})
         } else {
-            const stream = fs.createReadStream(`${way}\\${e.split('}}')[0]}.html`, 'utf-8');
+            const stream = fs.createReadStream(path.join(way, `${e.split('}}')[0]}.html`), 'utf-8');
             let part = ''
             stream.on('data', chunk => part += chunk);
             stream.on('end', () => {
-                fs.appendFile(`${out}\\index.html`, part, err => { })
-                fs.appendFile(`${out}\\index.html`, e.split('}}')[1], err => { })
+                (async function () {
+                    const data = await fs.appendFile(path.join(out, 'index.html'), part+e.split('}}')[1], err => {})
+                    console.log(data)
+                  })()
             })
         }
     })
 });
+
+
 
 ///////////////////////////////////////////css/////////////////////////////////////
 
@@ -40,16 +44,16 @@ function findingFiles(way) {
         items.forEach(e => {
             if (e.isFile() === true) {
                 if (e.name.split('.')[1] === 'css' && e.name !== 'style.css') {
-                    const input = fs.createReadStream(`${way}\\${e.name}`, 'utf-8');
+                    const input = fs.createReadStream(path.join(way, e.name), 'utf-8');
                     let data = '';
                     input.on('data', chunk => data += chunk);
                     input.on('end', () => {
-                        fs.appendFile(`${path.join(__dirname, 'project-dist', 'style.css')}`, data, err => { })
+                        fs.appendFile(path.join(__dirname, 'project-dist', 'style.css'), data, err => { })
                     });
                 }
             }
             if (e.isFile() === false && e.name !== 'test-files') {
-                findingFiles(`${way}\\${e.name}`)
+                findingFiles(path.join(way, e.name))
             }
         })
     })
@@ -61,15 +65,15 @@ function assetsCopy(way) {
     fs.readdir(way, { withFileTypes: true }, (err, items) => {
         items.forEach(e => {
             if (e.isFile() === true) {
-                fs.mkdir(path.join(__dirname, 'project-dist' , 'assets', `${`${way}\\${e.name}`.split('\\')[10]}`), err => { })
+                fs.mkdir(path.join(__dirname, 'project-dist', 'assets', `${`${way}\\${e.name}`.split('\\')[10]}`), err => { })
                 fs.copyFile(
-                    `${way}\\${e.name}`,
+                    path.join(way, e.name),
                     path.join(__dirname, 'project-dist', 'assets', `${`${way}\\${e.name}`.split('\\')[10]}`, e.name),
                     err => { }
                 )
             }
             if (e.isFile() === false) {
-                assetsCopy(`${way}\\${e.name}`)
+                assetsCopy(path.join(way, e.name))
             }
         })
     })
