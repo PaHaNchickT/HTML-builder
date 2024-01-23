66281093fc82d11990986e84ae58269c95972d8f
Pavel <you@example.com>
2024-01-23 18:42:20 +0300

    feat: implement correct html gen func (23.01.24 18:42)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,9 +4,11 @@ const { exit } = require('process');
 const fss = require('fs/promises');
 const way = path.join(__dirname, 'components');
 const out = path.join(__dirname, 'project-dist');
+const aboutSec = [];
 fs.mkdir(out, (err) => {});
 fs.mkdir(path.join(out, 'assets'), (err) => {});
 fs.writeFile(path.join(out, 'index.html'), '', (err) => {});
+const comp = [];
 
 ////////////////////////////////////////test files//////////////////////////////////////////
 
@@ -36,38 +38,54 @@ streamAbout.on('data', (chunk) => (tempAbout += chunk));
 streamAbout.on('end', () => {
   fss.writeFile(
     path.join(__dirname, 'template.html'),
-    `${tempAbout.split('{{articles}}')[0]} {{about}} \n    {{articles}} ${tempAbout.split('{{articles}}')[1]}`,
+    `${tempAbout.split('{{articles}}')[0]} {{about}} \n    {{articles}} ${
+      tempAbout.split('{{articles}}')[1]
+    }`,
     (err) => {},
   );
-});
 
 ///////////////////////////////////////////html/////////////////////////////////////////////
 
-const stream = fs.createReadStream(
-  path.join(__dirname, 'template.html'),
-  'utf-8',
-);
-let temp = '';
-stream.on('data', (chunk) => (temp += chunk));
-stream.on('end', () => {
-  temp.split('{{').forEach((e, i) => {
-    if (i === 0) {
-      fs.appendFile(path.join(out, 'index.html'), e, (err) => {});
-    } else {
-      const stream = fs.createReadStream(
-        path.join(way, `${e.split('}}')[0]}.html`),
-        'utf-8',
-      );
-      let part = '';
-      stream.on('data', (chunk) => (part += chunk));
-      stream.on('end', () => {
-        fss.appendFile(
-          path.join(out, 'index.html'),
-          part + e.split('}}')[1],
-          (err) => {},
-        );
+  fs.readdir(way, (err, itms) => {
+    itms.forEach((fileName) => {
+      aboutSec.push(fileName.split('.')[0]);
+    });
+    fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
+      data.split('{{').forEach((sects, ind1) => {
+        switch (ind1) {
+          case 0: {
+            fss.appendFile(path.join(out, 'index.html'), sects, (err) => {});
+            break;
+          }
+          case 1: {
+            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
+              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
+            });
+            break;
+          }
+          case 2: {
+            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
+              fss.appendFile(path.join(out, 'index.html'), `${inf}`, (err) => {});
+            });
+            break;
+          }
+          case 3: {
+            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
+              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
+            });
+            break;
+          }
+          case 4: {
+            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
+              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
+            });
+            break;
+          }
+        }
+        sects.split('}}').forEach((el) => {
+        });
       });
-    }
+    });
   });
 });
 
