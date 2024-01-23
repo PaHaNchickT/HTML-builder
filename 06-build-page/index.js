a0846dc6c9b949cb6850d946887446686d2eed0f
Pavel <you@example.com>
2024-01-23 19:30:21 +0300

    feat: implement HTML gen func without fsPromises.cp() func (23.01.24 19:30)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const { exit } = require('process');
-const fss = require('fs/promises');
 const way = path.join(__dirname, 'components');
 const out = path.join(__dirname, 'project-dist');
 const aboutSec = [];
@@ -33,10 +32,13 @@ const streamAbout = fs.createReadStream(
   path.join(__dirname, 'template.html'),
   'utf-8',
 );
+
+///////////////////////////////////////////html/////////////////////////////////////////////
+
 let tempAbout = '';
 streamAbout.on('data', (chunk) => (tempAbout += chunk));
 streamAbout.on('end', () => {
-  fss.writeFile(
+  fs.writeFile(
     path.join(__dirname, 'template.html'),
     `${tempAbout.split('{{articles}}')[0]} {{about}} \n    {{articles}} ${
       tempAbout.split('{{articles}}')[1]
@@ -44,48 +46,35 @@ streamAbout.on('end', () => {
     (err) => {},
   );
 
-///////////////////////////////////////////html/////////////////////////////////////////////
+  let names = [];
 
-  fs.readdir(way, (err, itms) => {
-    itms.forEach((fileName) => {
-      aboutSec.push(fileName.split('.')[0]);
-    });
-    fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
-      data.split('{{').forEach((sects, ind1) => {
-        switch (ind1) {
-          case 0: {
-            fss.appendFile(path.join(out, 'index.html'), sects, (err) => {});
-            break;
+  fs.promises.readdir(way).then((items) => {
+    fs.readFile(
+      path.join(__dirname, 'template.html'),
+      'utf-8',
+      (err, dataHTML) => {
+        let filesData = [];
+
+        dataHTML.split('{{').forEach((el, ind) => {
+          if (ind === 0) {
+            filesData.push(el);
+          } else {
+            filesData.push(
+              fs.promises.readFile(
+                path.join(way, `${el.split('}}')[0]}.html`),
+                'utf8',
+                (err) => {},
+              ),
+            );
+            filesData.push(el.split('}}')[1]);
           }
-          case 1: {
-            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
-              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
-            });
-            break;
-          }
-          case 2: {
-            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
-              fss.appendFile(path.join(out, 'index.html'), `${inf}`, (err) => {});
-            });
-            break;
-          }
-          case 3: {
-            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
-              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
-            });
-            break;
-          }
-          case 4: {
-            fs.readFile(path.join(way, `${sects.split('}}')[0]}.html`), 'utf-8', (err, inf) => {
-              fss.appendFile(path.join(out, 'index.html'), `${inf}${sects.split('}}')[1]}`, (err) => {});
-            });
-            break;
-          }
-        }
-        sects.split('}}').forEach((el) => {
         });
-      });
-    });
+
+        Promise.all(filesData).then((data) => {
+          fs.promises.writeFile(path.join(out, 'index.html'), data.join(''));
+        });
+      },
+    );
   });
 });
 
